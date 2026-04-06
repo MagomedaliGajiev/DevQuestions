5c18733d51a279e2263395dc48a69d31819696a6
Магомедали Гаджиев <mag198421@gmail.com>
2026-04-06 10:30:39 +0300

    refactor: Изменение именования констант в перечислении Status
    Изменены имена констант в перечислении Status с PascalCase (например, Open) на UPPER_CASE (например, OPEN) для соответствия общепринятым соглашениям об именовании значений перечисления. Также добавлены XML-комментарии для каждого статуса. Значение по умолчанию для свойства Status в классе Report обновлено с Status.Open на Status.OPEN.

--- DevQuestions/src/DevQuestions.Domain/Reports/Status.cs
+++ DevQuestions/src/DevQuestions.Domain/Reports/Status.cs
@@ -2,8 +2,23 @@
 
 public enum Status
 {
-    Open,
-    InProgress,
-    Resolved,
-    Dismissed
+    /// <summary>
+    /// Статус открыт.
+    /// </summary>
+    OPEN,
+
+    /// <summary>
+    /// Статус в раблоте.
+    /// </summary>
+    IN_PROGRESS,
+
+    /// <summary>
+    /// Статус решен.
+    /// </summary>
+    RESOLVED,
+
+    /// <summary>
+    /// Статус закрыт.
+    /// </summary>
+    DISMISSED,
 }
